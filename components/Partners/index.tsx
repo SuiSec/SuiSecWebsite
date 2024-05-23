87a44f61a5197d5576ffec4ed2094d39b4990ad3
Tom <litao.chn@foxmail.com>
2024-05-23 10:36:13 +0800

    [feat] add suiet wallet

--- components/Partners/index.tsx
+++ components/Partners/index.tsx
@@ -7,6 +7,18 @@ const Partners = () => {
       <div className={styles.inner}>
         <h2 className="text-4xl font-semibold mb-10">Our Partners</h2>
         <div className={styles.featureList}>
+          <a href="https://suiet.app/" target="_blank">
+            <div className={styles.feature}>
+              <Image
+                src="/images/suiet.svg"
+                alt="suiet"
+                width={80}
+                height={80}
+              />
+              <h3>Suiet Wallet</h3>
+            </div>
+          </a>
+
           <a href="https://surf.tech/" target="_blank">
             <div className={styles.feature}>
               <Image
