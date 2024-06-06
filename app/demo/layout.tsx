575a7bfb2038b87d94768241435bdbb14bbdd9a9
Wulabalabo <cyberchenjw@gmail.com>
2024-06-07 01:14:59 +0800

    [fix] demo page & connect button css

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import {
+  SuiClientProvider,
+  WalletProvider,
+  createNetworkConfig,
+} from "@mysten/dapp-kit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getFullnodeUrl } from "@mysten/sui.js/client";
+import "@mysten/dapp-kit/dist/index.css";
+
+const queryClient = new QueryClient();
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+            <WalletProvider autoConnect={true}>
+              {children}
+            </WalletProvider>
+          </SuiClientProvider>
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,25 +1,13 @@
-import { SuiClientProvider, WalletProvider, createNetworkConfig } from "@mysten/dapp-kit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { getFullnodeUrl } from "@mysten/sui.js/client";
+'use client'
 
-const queryClient = new QueryClient();
-// Config options for the networks you want to connect to
-const { networkConfig } = createNetworkConfig({
-  localnet: { url: getFullnodeUrl("localnet") },
-  mainnet: { url: getFullnodeUrl("mainnet") },
-  testnet: { url: getFullnodeUrl("testnet") },
-});
+import { ConnectButton } from "@mysten/dapp-kit";
 
-const page = ({children}:{children:React.ReactNode}) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-        <WalletProvider autoConnect={true}>
-          {children}
-        </WalletProvider>
-      </SuiClientProvider>
-    </QueryClientProvider>
-  );
-};
+const page = () => {
+    return (
+        <div>
+           <ConnectButton></ConnectButton>
+        </div>
+    );
+}
 
-export default page;
+export default page
